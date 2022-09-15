02c9bdbc387b3803f5bf68ff179ba650cc474808
Dinar Khasanov <lespaul@live.ru>
2022-09-15 21:13:25 +0300

    123

--- UnityPluginAndSample/Assets/Test.cs
+++ UnityPluginAndSample/Assets/Test.cs
@@ -1,34 +1,7 @@
-using TzarGames.Tinkoff;
-using UnityEngine;
-
-public class Test : MonoBehaviour
-{
-    public string PublicKey;
-
-    public string TerminalKey;
-    public string Password;
-    public bool IsDevMode = false;
-    public bool IsDebugMode = false;
-    public bool EnableFPS = false;
+public TinkoffPaymentParameters Parameters;
 
     void Start()
     {
-        var paymentParams = new TinkoffPaymentParameters
-        {
-            Title = "Все задания + созвездия",
-            Description = "Разблокировка всех заданий и созвездий. Новые задания и созвездия будут бесплатными",
-            ClientEmail = "",
-            MoneyAmount = 10,
-            OrderId = "COSMOSEA_UNLOCK_ALL",
-            IsDebugMode = IsDebugMode,
-            IsDevMode = IsDevMode,
-            TerminalKey = TerminalKey,
-            PublicKey = PublicKey,
-            CustomerKey = "",
-            Password = Password,
-            EnableFps = EnableFPS
-        };
-        
-        TinkoffPaymentParameters.StartPaymentProcess(paymentParams);
+        TinkoffPaymentParameters.StartPaymentProcess(Parameters);
     }
 }
