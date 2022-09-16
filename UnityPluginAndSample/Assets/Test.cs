00795fc6981562c3d627ddcd1725fb3cf600d315
Dinar <lespaul@live.ru>
2022-09-16 13:29:07 +0300

    исправил ошибки и обновил игнор лист

--- UnityPluginAndSample/Assets/Test.cs
+++ UnityPluginAndSample/Assets/Test.cs
@@ -1,4 +1,9 @@
-public TinkoffPaymentParameters Parameters;
+using TzarGames.Tinkoff;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public TinkoffPaymentParameters Parameters;
 
     void Start()
     {
